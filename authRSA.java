00f8062de4251fec4280889058a253f2b5612552
mochi_toll <cathy.earring@gmail.com>
2021-07-06 00:04:27 +0900

    Sign add

--- authRSA.java
+++ authRSA.java
@@ -3,29 +3,54 @@ import java.util.*;
 
 class authRSA{
     public static void main(String args[]){
-        keyGen();
-        signatureGen();
-        signatureVeri();
+        Scanner scan=new Scanner(System.in);
+        SecureRandom rand=new SecureRandom();
+        KeyPair pairKey=keyGen(rand);
+        PublicKey PubKey=pairKey.getPublic();
+        PrivateKey PriKey=pairKey.getPrivate();
+        System.out.print("message:");
+        String msg=scan.next();
+        byte[] sign=signatureGen(msg,PriKey,rand);
+        signatureVeri(msg, sign, PubKey);
     }
-    public static void keyGen(){
+    public static KeyPair keyGen(SecureRandom rand){
         KeyPairGenerator KPGen=null;
         try{
             KPGen = KeyPairGenerator.getInstance("RSA");
         }catch(NoSuchAlgorithmException e){
-            return;
+            return null;
         }
-        SecureRandom rand=new SecureRandom();
         KPGen.initialize(2048,rand);
         KeyPair pairKey=KPGen.generateKeyPair();
         PublicKey PubKey=pairKey.getPublic();
         System.out.println("PublicKey:"+PubKey.getAlgorithm()+" "+PubKey.getFormat()+" "+PubKey.getEncoded());
         PrivateKey PriKey=pairKey.getPrivate();
         System.out.println("PrivateKey:"+PriKey.getAlgorithm()+" "+PriKey.getFormat()+" "+PriKey.getEncoded());
+        return pairKey;
     }
-    public static void signatureGen(){
-
+    public static byte[] signatureGen(String msg, PrivateKey PriKey,SecureRandom rand){
+        Signature sign=null;
+        byte[] RTSign=null;
+        try{
+            sign =Signature.getInstance("SHA256withRSA");
+            sign.initSign(PriKey,rand);
+            sign.update(msg.getBytes());
+            RTSign=sign.sign();
+            System.out.println("Sign:"+sign);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return RTSign;
     }
-    public static void signatureVeri(){
-
+    public static void signatureVeri(String msg, byte[] RTSign, PublicKey PubKey){
+        Signature signVery=null;
+        try{
+            signVery=Signature.getInstance("SHA256withRSA");
+            signVery.initVerify(PubKey);
+            signVery.update(msg.getBytes());
+            System.out.println(signVery.verify(RTSign));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
