04c08d0c816ec6be266121e0d2f2f3ae9d33958b
mochi_toll <cathy.earring@gmail.com>
2021-07-05 15:59:03 +0900

    first commit

---
+++ authRSA.java
@@ -0,0 +1,19 @@
+import java.security.*;
+import java.util.*;
+
+class authRSA{
+    public static void main(String args[]){
+        keyGen();
+        signatureGen();
+        signatureVeri();
+    }
+    public static void keyGen(){
+
+    }
+    public static void signatureGen(){
+
+    }
+    public static void signatureVeri(){
+
+    }
+}
